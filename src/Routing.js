771b6a0250a2677705cae479e0fae1f035568d5a
terrence-nolan <terrencenolan@protonmail.com>
2024-01-29 14:29:20 -0500

    warning fixes

--- src/Routing.js
+++ src/Routing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import AboutPage from './pages/AboutPage/AboutPage';
 import Layout from './components/Layout/Layout';
 import ExperiencePage from './pages/ExperiencePage/ExperiencePage';
