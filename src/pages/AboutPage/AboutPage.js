35926e5768f970e1edc5f8b87d41a215e24f852b
terrence-nolan <terrencenolan@protonmail.com>
2024-01-29 15:41:57 -0500

    delete readme and unused imports

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -1,10 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from './AboutPage.module.css';
 import images from "../../images/images";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
-import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
-import {Link} from "react-router-dom";
 import ConnectionLinks from "../../components/ConnectionLinks/ConnectionLinks";
 
 const AboutPage = () => {
