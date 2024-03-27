58fe67a22489904dbf656cc40f063ef40d75be6f
terrence-nolan <terrencenolan@protonmail.com>
2024-03-26 23:17:23 -0400

    add more to bio

--- src/pages/BioSection/BioSection.js
+++ src/pages/BioSection/BioSection.js
@@ -4,8 +4,12 @@ import styles from './BioSection.module.css';
 export const BioSection = () => {
     return(
         <section className={styles.bioContainer}>
-            <p className={styles.text}>I am a senior CS student looking for a full time SWE role,
-                with a budding passion for front-end engineering.</p>
+            <p className={styles.text}>Hi, my name is Terrence Nolan. I am a senior CS student looking for a
+                full time SWE role, with a budding passion for front-end engineering and building
+                aesthetic projects.</p>
+            <p className={styles.text}>Outside of programming, I enjoy carrying out my imaginative designs in a more
+                analog fashion through drawing and painting. I also enjoy running (I just recently completed my
+                first half marathon), playing soccer, and teaching myself the acoustic guitar.</p>
             <span className={styles.text}><span className={styles.blueText}>Elective Coursework:</span> Applied Machine Learning, Computer Graphics,
                             Overview of Cybersecurity</span>
         </section>
