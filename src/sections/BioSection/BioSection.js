30e7dde4d03f3941db6328e2f1eecbd62266a587
terrence-nolan <terrencenolan@protonmail.com>
2024-08-13 18:33:35 -0400

    update bio

--- src/sections/BioSection/BioSection.js
+++ src/sections/BioSection/BioSection.js
@@ -2,18 +2,19 @@ import React from 'react';
 import styles from './BioSection.module.css';
 
 export const BioSection = () => {
-    return(
+    return (
         <section className={styles.bioContainer}>
             <span className={styles.sectionTitle}>About Me</span>
-            <div className={styles.underline}/>
-            <p className={styles.text}>Hi, my name is Terrence Nolan. I am a senior CS student looking for a
-                full time SWE role, with a budding passion for front-end engineering and building
-                aesthetic projects.</p>
-            <p className={styles.text}>Outside of programming, I enjoy carrying out my imaginative designs in a more
-                analog fashion through drawing and painting. I also enjoy running (I just recently completed my
-                first half marathon), playing soccer, and teaching myself the acoustic guitar.</p>
-            <span className={styles.text}><span className={styles.blueText}>Elective Coursework:</span> Applied Machine Learning, Computer Graphics,
-                            Overview of Cybersecurity</span>
+            <div className={styles.underline} />
+            <p className={styles.text}>
+                Hi, my name is TJ. I just graduated from college and I'm currently working my
+                first adult job as a software engineer for 5 Stones.
+                <br />
+                <br />
+                Outside of programming, I enjoy carrying out my imaginative designs in a more
+                analog fashion through drawing. I also enjoy running (I just recently completed my
+                first half marathon), playing soccer, and teaching myself the acoustic guitar.
+            </p>
         </section>
     )
 }
